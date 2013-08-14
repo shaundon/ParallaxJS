4cb173fc20cd1dcabec3b04ddac661b51ee52b39
Shaun Donnelly <hello@sdonnelly.co.uk>
2013-08-14 21:23:52 +0100

    Initial Commit
    Includes a demo, plus parallax.js and parallax.css. More to come.

---
+++ parallax.js
@@ -0,0 +1,20 @@
+window.ondeviceorientation = function (event) {
+		
+	var gamma = Math.round(event.gamma);
+	var beta = Math.round(event.beta);
+	var direction = Math.round(event.alpha);
+		
+	Parallax(gamma, beta, direction);
+};
+		
+function Parallax(gamma, beta, direction) {
+		
+	$(".parallax").css("margin-top", beta + "px");
+	$(".parallax").css("margin-left", gamma + "px");
+			
+	$(".parallax.more").css("margin-top", Math.round(beta*2) + "px");
+	$(".parallax.more").css("margin-left", Math.round(gamma*2) + "px");
+			
+	$(".parallax.less").css("margin-top", Math.round(beta/2) + "px");
+	$(".parallax.less").css("margin-left", Math.round(gamma/2) + "px");
+}
